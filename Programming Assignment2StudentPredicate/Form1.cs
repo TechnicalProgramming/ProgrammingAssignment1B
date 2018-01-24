1ea1aff56de334610baaa52d294a01d3d9280189
Que2701 <phemelo27@gmail.com>
2018-01-24 09:16:24 +0200

    Added functionality for the clear button

--- Programming Assignment2StudentPredicate/Form1.cs
+++ Programming Assignment2StudentPredicate/Form1.cs
@@ -12,9 +12,28 @@ namespace Programming_Assignment2StudentPredicate
 {
     public partial class Form1 : Form
     {
+        // Class variables 
+        ResetFormComponents resetFormComponents = ResetFormComponents.Instance;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            // Call a function to reset the components
+            resetFormComponents.ClearComponents(practicalsTextBox, term1TextBox, term2TextBox, additionalTextBox, projectTextBox, calculateButton, clearButton, predicateResultsLabel, predicateNeededLabel);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Call a function to reset the components
+            resetFormComponents.ClearComponents(practicalsTextBox, term1TextBox, term2TextBox, additionalTextBox, projectTextBox, calculateButton, clearButton, predicateResultsLabel, predicateNeededLabel);
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
---
+++ Programming Assignment2StudentPredicate/ResetFormComponents.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Programming_Assignment2StudentPredicate
+{
+    /// <summary>
+    /// Resets UI components to default
+    /// </summary>
+    public sealed class ResetFormComponents
+    {
+        #region Fields
+        private static bool isReset = false;
+        private static volatile ResetFormComponents instance;
+        private static object syncRoot = new object();
+        #endregion
+
+        #region Constructor
+        /// <summary>
+        /// Private constructor
+        /// </summary>
+        private  ResetFormComponents() { }
+        #endregion
+
+        #region Properties
+        public static ResetFormComponents Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (syncRoot)
+                    {
+                        if (instance == null)
+                            instance = new ResetFormComponents();
+                    }
+                }
+                return instance;
+            }
+        }
+
+        /// <summary>
+        /// Successful reset of components
+        /// </summary>
+        public bool IsReset { get {return isReset; } set { isReset = value; } }
+        #endregion
+
+        #region Methods
+        public bool ClearComponents(TextBox praticalTotal, TextBox term1, TextBox term2, TextBox additional, TextBox project,Button calculateButton, Button clearButton, Label predicateResults, Label predicateNeeded)
+        {
+            try
+            {
+                // Clear the text properties 
+                praticalTotal.Text = "";
+                term1.Text = "";
+                term2.Text = "";
+                additional.Text = "";
+                project.Text = "";
+                // Default the labels text and colour
+                predicateResults.Text = "Your predicate is: 00.0";
+                predicateResults.ForeColor = System.Drawing.Color.Black;
+
+                predicateNeeded.Text = "You need 00.0 in the exam to pass";
+                predicateNeeded.ForeColor = System.Drawing.Color.Black;
+                // Disable the buttons
+                calculateButton.Enabled = false;
+                clearButton.Enabled = false;
+                return true;
+            }catch(Exception exception)
+            {
+                MessageBox.Show("Error",exception.Message,MessageBoxButtons.OK,MessageBoxIcon.Error);
+                return false;
+            }
+        }
+        #endregion
+    }
+}
