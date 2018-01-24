ad8a564c38e150c503323d1ed7c0c4616c0bfbc5
Que2701 <phemelo27@gmail.com>
2018-01-24 22:53:44 +0200

    Added a base class calculator

---
+++ Programming Assignment2StudentPredicate/BaseCalculator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programming_Assignment2StudentPredicate
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    abstract class BaseCalculator
+    {
+        #region Fields
+
+        #endregion
+
+        #region
+        /// <summary>
+        /// Abstract method to calculate student predicate
+        /// </summary>
+        /// <param name="practicalTest"></param>
+        /// <param name="termTest1"></param>
+        /// <param name="termTest2"></param>
+        /// <param name="additionalTest"></param>
+        /// <param name="projectTest"></param>
+        /// <returns></returns>
+        public abstract float CalculatePredicate(int practicalTest, int termTest1, int termTest2, int additionalTest, int projectTest);
+        /// <summary>
+        /// Abstract method to calculate the needed 
+        /// </summary>
+        /// <param name="practicalTest"></param>
+        /// <param name="termTest1"></param>
+        /// <param name="termTest2"></param>
+        /// <param name="additionalTest"></param>
+        /// <param name="projectTest"></param>
+        /// <returns></returns>
+        public abstract float CalculatePredicateNeeded(int practicalTest, int termTest1, int termTest2, int additionalTest, int projectTest);
+        
+        #endregion
+    }
+}
---
+++ Programming Assignment2StudentPredicate/BaseStudentTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programming_Assignment2StudentPredicate
+{
+    /// <summary>
+    /// Base class for all student tests
+    /// </summary>
+    abstract class BaseStudentTests
+    {
+        #region Fields
+
+        protected int practicalTest = 0;
+        protected int termTest1 = 0;
+        protected int termTest2 = 0;
+        protected int additionalTest = 0;
+        protected int projectTest = 0;
+        protected float predicateResult = 0f;
+        protected float predicateNeeded = 0f;
+
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Practical test marks
+        /// </summary>
+        public abstract int PracticalTest { get; }
+        /// <summary>
+        /// Term test 1 marks
+        /// </summary>
+        public abstract int TermTest1 { get; }
+        /// <summary>
+        /// Term test 2 marks
+        /// </summary>
+        public abstract int TermTest2 { get; }
+        /// <summary>
+        /// Additional test marks
+        /// </summary>
+        public abstract int AdditionalTest { get; }
+        /// <summary>
+        /// Project test marks
+        /// </summary>
+        public abstract int ProjectTest { get; }
+        /// <summary>
+        /// Predicate marks
+        /// </summary>
+        public abstract float PredicateResults { get; }
+        /// <summary>
+        /// Predicate needed 
+        /// </summary>
+        public abstract float PredicateNeeded { get; }
+
+        #endregion
+    }
+}
